9ce06095d2ac6f2a0d6e52866942f5c239dc5628
dunghenry <trandungksnb6930082001@gmail.com>
2022-08-04 17:16:44 +0700

    Remove graphql-ts

--- graphql-ts/server/src/resolvers/resolvers.ts
+++
@@ -1,9 +0,0 @@
-const resolvers  = {
-    Query : {
-        books: () =>{
-            return [{id: 1, title: "De men phieu lieu ki", genre: "Adventure"}]
-        }
-    }
-}
-
-export default resolvers;
